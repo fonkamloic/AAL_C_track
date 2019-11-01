02ce53c5ffcdce399e5ab1d7f52d4f269fc1215b
fonkamloic <fonkamloic@gmail.com>
2019-11-01 20:25:06 +0100

    create and modify your own library

---
+++ U7.2_create_library.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main(void){
+    printf("gcc -std=c11 -Wall -fmax-errors=10 -Wextra -c libimage.c -o libimage.so");
+    printf("gcc -std=c11 -Wall -fmax-errors=10 -Wextra -limage superimage.c -o superimage\n");
+    return 0;
+}
+
